135b22af0bc08163583e9b4b258795c8ecb688d2
naniewiadoms5 <naniewiadoms@cps.edu>
2023-12-08 10:38:26 -0600

    finished a6 part 1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -11,14 +11,47 @@ y = data["Blood Pressure"].values
 x = x.reshape(-1,1)
 
 # Create the model
+model = LinearRegression().fit(x, y)
 
 # Find the coefficient, bias, and r squared values. 
-# Each should be a float and rounded to two decimal places. 
+# Each should be a float and rounded to two decimal places.
 
-
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+# calculating r-squared here
+r_squared = model.score(x, y)
+ 
 # Print out the linear equation and r squared value
 
-# Predict the the blood pressure of someone who is 43 years old.
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+
+# Predict the blood pressure of someone who is 43 years old.
+
+x_predict = 43
+prediction = model.predict([[x_predict]])
+
 # Print out the prediction
 
+print(f"Prediction when x is {x_predict}: {prediction}")
+
 # Create the model in matplotlib and include the line of best fit
+# sets the size of the graph
+plt.figure(figsize=(6,4))
+
+# creates a scatter plot of originial data in purple
+# and the predicted data in blue
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
+
+# label the axes
+plt.xlabel("Age (years)")
+plt.ylabel("Blood Pressure (mmHg)")
+plt.title("Blood Pressure vs. Age")
+
+# plot the line of best fit in red and label the line
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+# show the plot and legend
+plt.legend()
+plt.show()
--- part1-linear-regression/part1_writeup.md
+++ part1-linear-regression/part1_writeup.md
@@ -6,6 +6,12 @@ After completing `a6_part1.py` answer the following questions
 
 1. What is the r squared value?  What does this say about this lenear regression model?
 
+The r^2 value is the coefficient of determination; the model got 0.0.6257932855322312. It is calculated based on a line of best fit, measuring the distance between each point to that line. It will show how well the model the data points got predicted by the model based on the linear relationship.
+
 2. According to your model, what is the predicted systolic blood pressure for a person iwho is 43 years old?
 
+When a person is 43 years old the predicted systolic blood pressure would be, 137.46185286 mmHg. 
+
 3. How accurate do you think this model's predictions are?  Do you think this model is accurate enough to reliably predict someone's blood pressure based on their age?  Why or why not?  And if not, what could improve the model?
+
+Based on the scatter plot printed by matplotlib and the r^2 value, I see that the points are only a little correlated to the line of best fit, despite there being a relationship between aging and blood pressure. Some points are far outliers, while most are close, making the r^2 value not too bad.
