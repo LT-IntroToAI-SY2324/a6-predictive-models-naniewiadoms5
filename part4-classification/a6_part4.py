9fc5c8aa81dbfe58bb48dabdd9e31749e24865b9
naniewiadoms5 <naniewiadoms@cps.edu>
2023-12-19 09:11:14 -0600

    finished a6 part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -4,7 +4,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("suv_data.csv")
+data = pd.read_csv("C:/Users/naniewiadoms@cps.edu/Documents/IntrotoAI/a6-predictive-models-naniewiadoms5/part4-classification/suv_data.csv")
 data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 
 x = data[["Age", "EstimatedSalary", "Gender"]].values
@@ -12,20 +12,22 @@ y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
 
+print("x values: ")
 print(x)
+print("y values: ")
 print(y)
 
 # Step 2: Standardize the data using StandardScaler, 
 
-scaler = StandardScaler().fit(x)
+# scaler = StandardScaler()
 
 # Step 3: Transform the data
 
-x = scaler.transform(x)
+# scaled_x = scaler.fit_transform(x)
 
 # Step 4: Split the data into training and testing data
 
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2) #scaled_x insted of x for actual
 
 # Step 5 & 6: Fit the data + create a LogsiticRegression object and fit the data
 
@@ -33,28 +35,20 @@ model = linear_model.LogisticRegression().fit(x_train, y_train)
 
 # Step 7: Print the score to see the accuracy of the model
 
-print("Accuracy:", model.score(x_test, y_test))
-print("*************")
-print("Testing Results:")
-print("")
-print(y_test)
-for index in range(len(x_test)):
-    x = x_test[index]
-    x = x.reshape(-1, 3)
-    y_pred = int(model.predict(x))
-    actual = y_test[index]
+accuracy = model.score(x_test, y_test)
+print("\n Accuracy of model:", accuracy)
 
+y_pred = model.predict(x_test)
 
-# Step 8: Print out the actual ytest values and predicted y values
-# based on the xtest data
+print("\n Actual ytest values:")
+print(y_test)  
 
-print(y_pred) 
-print(actual)
+print("\n Predicted y values:")
+print(y_pred)
 
-# prediction
-my_person = [[34, 56000, 1]]
-numpy_array = np.array(my_person)
+# my_person = [[34, 56000, 1]]
+# numpy_array = np.array(my_person)
 
-my_person_scaled = scaler.transform(my_person)
-my_prediction = model.predict(my_person_scaled)
-print(my_prediction)
+# my_person_scaled = scaler.transform(my_person)
+# my_prediction = model.predict(my_person_scaled)
+# print(my_prediction)
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -6,15 +6,17 @@ After completing `a6_part4.py` answer the following questions
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
 
-With the StandardScaler commented out, the model has an accuracy of 64% or 0.64. This model is not accurate because the data is not standardized, meaning that the input data is with high variance. Usually image processing would be done to set images to the same amount of pixels and other techniques, but the StandardScaler balances the input data on its own.
+With the StandardScaler commented out, the model has an accuracy of 61% or 0.6125. This model is not accurate because the data is not standardized, meaning that the input data is with high variance. Usually image processing would be done to set images to the same amount of pixels and other techniques, but the StandardScaler balances the input data on its own.
 
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
 
-The model got an accuracy rate of 0.86. Now that the data is balanced and can be compared to the same level. This model should be used for predicting if an SUV should be bought because the model receives a high accuracy.
+The model got an accuracy rate of 0.8375, now that the data is balanced and can be compared to the same level. This model should be used for predicting if an SUV should be bought because the model receives a high accuracy.
 
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
 
-
+Looking through the predicted and actual results, the model seemed to be only predicting the purchased or y-values incorrectly infrequently. One pattern I see is that the model does not predict an inaccurate value consecutively, or in a row. 
 
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
+
+The prediction I got was [0] which means no.
 
